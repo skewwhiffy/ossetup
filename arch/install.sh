1ba7e77dcc93cb14c93e5eca833e7c6d9de5b7da
Kenny Hung <kennysubs+github@outlook.com>
2019-10-15 10:44:06 +0100

    Rank mirrors

--- arch/install.sh
+++ arch/install.sh
@@ -2,9 +2,10 @@ pacman -Sy pacman-contrib
 timedatectl set-ntp true
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-rm -f /etc/pacman.d/mirrorlist.uk
-cat /etc/pacman.d/mirrorlist.backup | grep -A 1 "United Kingdom" | grep "[^\-]" >> /etc/pacman.d/mirrorlist.uk
+cat /etc/pacman.d/mirrorlist.backup | grep -A 1 "United Kingdom" | grep "[^\-]" > /etc/pacman.d/mirrorlist.uk
+rankmirrors /etc/pacman.d/mirrorlist.uk > /etc/pacman.d/mirrorlist
 
 pacstrap /mnt base linux linux-firmware base-devel dhcpcd git
 
 genfstab -U /mnt >> /mnt/etc/fstab
+
