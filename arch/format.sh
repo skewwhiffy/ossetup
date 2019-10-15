ae5db4a29840295310b587e5e95dd05579576bf6
Kenny Hung <kennysubs+github@outlook.com>
2019-10-15 10:11:07 +0100

    format working

--- arch/format.sh
+++ arch/format.sh
@@ -1,3 +1,3 @@
 parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart primary 512MiB -17.2GiB
-parted /dev/sda -- mkpart primary linux-swap -17.2GiB 100%
+parted /dev/sda -- mkpart primary 512MiB -16GiB
+parted /dev/sda -- mkpart primary linux-swap -16GiB 100%
