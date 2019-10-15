8f2fd44c9d7f29b6a6fe9b766b6fccca47898512
Kenny Hung <kennysubs+github@outlook.com>
2019-10-15 18:42:08 +0100

    Setup new install

--- arch/install.sh
+++ arch/install.sh
@@ -6,7 +6,7 @@ cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 cat /etc/pacman.d/mirrorlist.backup | grep -A 1 "United Kingdom" | grep "[^\-]" > /etc/pacman.d/mirrorlist.uk
 rankmirrors /etc/pacman.d/mirrorlist.uk > /etc/pacman.d/mirrorlist
 
-pacstrap /mnt base linux linux-firmware base-devel dhcpcd git
+pacstrap /mnt base linux linux-firmware base-devel dhcpcd git neovim grub efibootmgr
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
---
+++ arch/setup.sh
@@ -0,0 +1,20 @@
+ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+hwclock --systohc
+
+cp /etc/locale.gen /etc/locale.gen.backup
+echo en_GB.UTF-8 UTF-8 > /etc/locale.gen
+locale-gen
+echo LANG=en_GB.UTF-8 > /etc/locale.conf
+echo KEYMAP=uk > /etc/vconsole.conf
+echo kenny-linux > /etc/hostname
+echo 127.0.0.1 localhost >> /etc/hosts
+echo ::1 localhost >> /etc/hosts
+echo 127.0.1.1 kenny-linux.localdomain kenny-linux >> /etc/hosts
+mkinitcpio -P
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo Set root password
+passwd
+
+echo Reboot. You should get a working system.
