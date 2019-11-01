496aab2867d5e76c3f5d1b5d966647d02d0e7512
Kenny Hung <kennysubs+github@outlook.com>
2019-11-01 11:31:38 +0000

    Add conky to apps

--- arch/add.common.sh
+++ arch/add.common.sh
@@ -5,15 +5,15 @@ git clone https://aur.archlinux.org/yay.git
 makepkg -Si
 
 echo Installing common utilities from AUR
-yay -Syu yadm docker ttf-iosevka aic94xx-firmware wd719x-firmware
+yay -Syu yadm docker ttf-iosevka aic94xx-firmware wd719x-firmware jetbrains-toolbox 
 yay -Syu dropbox-gtk rtl88xxau-aircrack-dkms-git spotblock-git wkhtmltopdf-static
 sudo groupadd docker
 sudo usermod -aG docker kenny
 systemctl --user enable spotblock
 
 echo Installing common utilities
-sudo pacman -Syu tk inetutils gnome-disk-utility openssh firefox neovim wmctrl chromium jetbrains-toolbox usbutils
-sudo pacman -Syu xclip code flatpak zsh keepassxc mame maven curl wget
+sudo pacman -Syu tk inetutils gnome-disk-utility openssh firefox neovim wmctrl chromium usbutils
+sudo pacman -Syu xclip code flatpak zsh keepassxc mame maven curl wget conky
 sudo pacman -Syu noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
 
 echo Installing some flatpaks
