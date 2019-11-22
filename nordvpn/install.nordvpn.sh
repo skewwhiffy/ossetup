ec10f3fe660e0b0bbbc60140c072bf52626c6593
Kenny Hung <kennysubs+github@outlook.com>
2019-11-22 19:41:46 +0000

    Install nordvpn script

--- nordvpn/install.nordvpn.sh
+++ nordvpn/install.nordvpn.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 fileName=$(curl https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/ | grep amd64 | grep deb | sort -r | head -n 1 | grep -oP ">.*<" | sed "s/>//" | sed "s/<//")
 url=https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/$fileName
+rm -f nordvpn.deb
 wget -O nordvpn.deb $url
+rm -rf workspace
+mkdir ./workspace
+bsdtar -O -xf *.deb data.tar.xz | bsdtar -C ./workspace -xJf -
+sudo cp -r ./workspace/var/* /var
+sudo cp -r ./workspace/usr/* /usr
