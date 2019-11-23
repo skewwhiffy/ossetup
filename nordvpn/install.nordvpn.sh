d063d574d12484170579e7730b54fb1eb4806ebb
Kenny Hung <kennysubs+github@outlook.com>
2019-11-23 06:33:01 +0000

    Deduplicate nordvpn installation script

--- nordvpn/install.nordvpn.sh
+++ nordvpn/install.nordvpn.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
+echo Getting latest version of nordvpn
 fileName=$(curl https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/ | grep amd64 | grep deb | sort -r | head -n 1 | grep -oP ">.*<" | sed "s/>//" | sed "s/<//")
 url=https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/$fileName
+
+echo Downloading latest version of nordvpn
 rm -f nordvpn.deb
 wget -O nordvpn.deb $url
+
+echo Extracting package
 rm -rf workspace
 mkdir ./workspace
 bsdtar -O -xf *.deb data.tar.xz | bsdtar -C ./workspace -xJf -
+
+echo Installing nordvpn. I might need your password.
 sudo cp -r ./workspace/var/* /var
 sudo cp -r ./workspace/usr/* /usr
+
+echo Done.
+echo To run as a daemon: sudo nordvpnd
+echo nordvpn should be installed.
