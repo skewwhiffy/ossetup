6a4ab331a4d8a100db2cc0deb258c636228e8c8a
Kenny Hung <kennysubs+github@outlook.com>
2019-12-08 17:58:08 +0000

    Auto calculate swap on artix/format.sh

--- artix/format.sh
+++ artix/format.sh
@@ -1,9 +1,18 @@
+
+totalk=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
+totalm=$(($totalk / 1024))
+totalg=$(($totalm / 1024))
+swap=$(($totalg + 1))
+echo Creating "$swap"GiB of swap
+
 pacman -Sy --noconfirm parted
-parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart primary 512MiB -16GiB
-parted /dev/sda -- mkpart primary linux-swap -16GiB 100%
-parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
-parted /dev/sda -- set 3 boot on
+swapoff -a
+
+parted -a optimal -s /dev/sda -- mklabel gpt
+parted -a optimal -s /dev/sda -- mkpart primary 512MiB -"$swap"GiB
+parted -a optimal -s /dev/sda -- mkpart primary linux-swap -"$swap"GiB 100%
+parted -a optimal -s /dev/sda -- mkpart ESP fat32 1MiB 512MiB
+parted -a optimal -s /dev/sda -- set 3 boot on
 
 mkfs.ext4 -L artix /dev/sda1
 mkswap -L swap /dev/sda2
