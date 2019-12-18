21105eaaf2db582af1ec0b7e4b281c5f3bbe129e
Kenny Hung <kennysubs+github@outlook.com>
2019-12-18 14:27:05 +0000

    Maybe make add.gnome script work for Arch

--- arch/add.gnome.sh
+++ arch/add.gnome.sh
@@ -1,8 +1,8 @@
 echo This currently doesn't work. Why? Who knows?
 
 sudo pacman -Syu
-sudo pacman -Syu linux-headers nvidia terminator
-sudo pacman -Syu gnome gnome-extra
+sudo pacman -Syu linux-headers nvidia terminator \
+  xorg gnome gnome-extra
 sudo systemctl enable gdm
 
 echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
