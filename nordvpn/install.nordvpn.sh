31c31b288b872b5a2d0fb2eba20f0156d19340e2
Kenny Hung <kennysubs+github@outlook.com>
2019-12-19 15:19:52 +0000

    install.nordvpn.sh working for void

--- nordvpn/install.nordvpn.sh
+++ nordvpn/install.nordvpn.sh
@@ -14,9 +14,8 @@ bsdtar -O -xf *.deb data.tar.xz | bsdtar -C ./workspace -xJf -
 
 echo Installing nordvpn. I might need your password.
 sudo cp -r ./workspace/var/* /var
-sudo cp -r ./workspace/usr/bin /usr/bin
+sudo cp -r ./workspace/usr/bin/* /usr/bin
 sudo cp -r ./workspace/usr/sbin/* /usr/sbin/
-sudo cp -r ./workspace/usr/lib /usr/sbin
 sudo chmod 0755 /usr/bin/nordvpn
 
 echo Done.
