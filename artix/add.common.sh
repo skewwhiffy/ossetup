5ff1e4963f0fd96679e47829ff7e37e3a9cc5e8d
Kenny Hung <kennysubs+github@outlook.com>
2020-02-01 19:24:09 +0000

    Install IntelliJ directly, rather than through toolbox

--- artix/add.common.sh
+++ artix/add.common.sh
@@ -14,7 +14,7 @@ echo Installing common stuff
 yay -Syu --noconfirm tk inetutils gnome-disk-utility openssh wmctrl chromium usbutils \
 	xclip code mame maven curl wget conky the_silver_searcher \
 	noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fzf \
-	docker ttf-iosevka aic94xx-firmware wd719x-firmware jetbrains-toolbox \
+	docker ttf-iosevka aic94xx-firmware wd719x-firmware intellij-idea-ce \
 	rtl88xxau-aircrack-dkms-git wkhtmltopdf-static docker-runit \
 	spotify blockify zoom dbeaver postman wget curl chromium onedrive-abraunegg
 
