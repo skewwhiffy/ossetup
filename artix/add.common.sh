3d4913da051c607ac1580c75ca6bd4f43dab2a34
Kenny Hung <kennysubs+github@outlook.com>
2020-02-01 20:58:06 +0000

    Make script idempotent and nice, and remove Spotify because it always fails

--- artix/add.common.sh
+++ artix/add.common.sh
@@ -5,17 +5,17 @@ if [[ ! -d yay ]]; then
     git clone https://aur.archlinux.org/yay.git
 fi
 cd yay
-makepkg -si --noconfirm
+makepkg -si --noconfirm --needed
 
 echo Installing the first few so that you can get on with other stuff.
-yay -Syu --noconfirm yadm dropbox firefox neovim zsh keepassxc rhythmbox
+yay -Syu --noconfirm --needed yadm dropbox firefox neovim zsh keepassxc rhythmbox
 
 echo Installing common stuff
-yay -Syu --noconfirm tk inetutils gnome-disk-utility openssh wmctrl chromium usbutils \
-	xclip code mame maven curl wget conky the_silver_searcher \
-	noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fzf \
-	docker ttf-iosevka aic94xx-firmware wd719x-firmware intellij-idea-ce \
-	rtl88xxau-aircrack-dkms-git wkhtmltopdf-static docker-runit \
-	spotify blockify zoom dbeaver postman wget curl chromium onedrive-abraunegg
+yay -Syu --noconfirm --needed tk inetutils gnome-disk-utility openssh wmctrl chromium usbutils
+yay -Syu --noconfirm --needed xclip code mame maven curl wget conky the_silver_searcher
+yay -Syu --noconfirm --needed noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fzf
+yay -Syu --noconfirm --needed docker ttf-iosevka aic94xx-firmware wd719x-firmware intellij-idea-ce
+yay -Syu --noconfirm --needed rtl88xxau-aircrack-dkms-git wkhtmltopdf-static docker-runit
+yay -Syu --noconfirm --needed zoom dbeaver postman wget curl chromium onedrive-abraunegg
 
 echo Reboot. You should have quite a lot of your stuff setup now. See dev folder to set up dev tools.
