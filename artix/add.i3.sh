a5b13d08640a43f4fe98e6e856ab6d287aea309f
Kenny Hung <kennysubs+github@outlook.com>
2020-02-01 22:20:30 +0000

    Install dmenu with i3

--- artix/add.i3.sh
+++ artix/add.i3.sh
@@ -1,7 +1,7 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -Syu --noconfirm linux-headers nvidia terminator
 sudo pacman -Syu --noconfirm lightdm lightdm-gtk-greeter lightdm-runit
-sudo pacman -Syu --noconfirm i3 j4-dmenu-desktop pavucontrol
+sudo pacman -Syu --noconfirm i3 j4-dmenu-desktop pavucontrol dmenu
 sudo ln -s /etc/runit/sv/lightdm /run/runit/service
 
 echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
