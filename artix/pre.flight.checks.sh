79738fe562abfcef4206c41ab46feb4787ee4256
Kenny Hung <kennysubs+github@outlook.com>
2020-02-10 16:49:12 +0000

    Rename format to install

--- artix/pre.flight.checks.sh
+++ artix/pre.flight.checks.sh
@@ -9,23 +9,3 @@ then
   exit 1
 fi
 echo System disk is $disk
-
-echo Getting user password
-export password=$(grep -Po "(?<=^password=).+" config)
-if [ $password == USER_PASSWORD ]
-then
-  echo "***** ERROR *****"
-  echo "You need to set the user password in config"
-  exit 1
-fi
-echo User password is set
-
-echo Getting root password
-export root_password=$(grep -Po "(?<=^root_password=).+" config)
-if [ $root_password == ROOT_PASSWORD ]
-then
-  echo "***** ERROR *****"
-  echo "You need to set the root password in config"
-  exit 1
-fi
-echo Root password is set
