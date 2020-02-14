0ecae5da0c83c387e9cf56f2105a84d3f04664ea
Kenny Hung <kennysubs+github@outlook.com>
2020-02-14 15:56:30 +0000

    Add mate script for Artix

--- artix/add.mate.sh
+++ artix/add.mate.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -Syu --noconfirm xorg linux-headers nvidia-dkms terminator
 sudo pacman -Syu --noconfirm lightdm lightdm-gtk-greeter lightdm-runit
-sudo pacman -Syu --noconfirm cinnamon
+sudo pacman -Syu --noconfirm mate
 sudo ln -s /etc/runit/sv/lightdm /run/runit/service
 
 echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
