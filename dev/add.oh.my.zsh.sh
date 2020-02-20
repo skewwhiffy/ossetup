a50287120674dc6f497bf683e038e679f3f3fa9a
Kenny Hung <kennysubs+github@outlook.com>
2020-02-20 20:48:41 +0000

    Add zinit to zsh install script

--- dev/add.oh.my.zsh.sh
+++ dev/add.oh.my.zsh.sh
@@ -1,2 +1,4 @@
 echo Installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo Installing zinit
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
