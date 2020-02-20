176f8003f0194fa8ffff433b766414e7f2971f80
Kenny Hung <kennysubs+github@outlook.com>
2020-02-20 18:28:49 +0000

    Fix use of sudo yay in artix cinnamon script

--- artix/add.cinnamon.sh
+++ artix/add.cinnamon.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sudo pacman -Syu --noconfirm
-sudo pacman -Syu --noconfirm xorg linux-headers nvidia-dkms terminator
-sudo pacman -Syu --noconfirm lightdm lightdm-gtk-greeter lightdm-runit
-sudo pacman -Syu --noconfirm cinnamon
-sudo yay -Syu mint-themes redmond-themes-git
+sudo pacman -Syu --noconfirm --needed
+sudo pacman -Syu --noconfirm --needed xorg linux-headers nvidia-dkms terminator
+sudo pacman -Syu --noconfirm --needed lightdm lightdm-gtk-greeter lightdm-runit
+sudo pacman -Syu --noconfirm --needed cinnamon
+yay -Syu --noconfirm --needed mint-themes redmond-themes-git
 sudo ln -s /etc/runit/sv/lightdm /run/runit/service
 
 echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
