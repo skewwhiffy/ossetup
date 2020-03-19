091416f5060d64f773b65114802d980f2b28a3f2
Kenny Hung <kennysubs+github@outlook.com>
2020-03-19 11:46:16 +0000

    Install VLC

--- artix/add.common.sh
+++ artix/add.common.sh
@@ -48,6 +48,7 @@ yay -Syu --noconfirm --needed the_silver_searcher
 yay -Syu --noconfirm --needed tk
 yay -Syu --noconfirm --needed ttf-iosevka
 yay -Syu --noconfirm --needed usbutils
+yay -Syu --noconfirm --needed vlc
 yay -Syu --noconfirm --needed wd719x-firmware
 yay -Syu --noconfirm --needed wget
 yay -Syu --noconfirm --needed wget
