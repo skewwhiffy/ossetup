1cda55558e658f7da830c0f3dcb4d674566174f3
Kenny Hung <kennysubs+github@outlook.com>
2020-04-04 10:37:36 +0100

    Add mate-extra to artix

--- artix/add.mate.sh
+++ artix/add.mate.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+source ./pre.flight.checks.sh
 sudo pacman -Syu --noconfirm
 sudo pacman -Syu --noconfirm xorg linux-headers nvidia-dkms terminator
-sudo pacman -Syu --noconfirm lightdm lightdm-gtk-greeter lightdm-runit
+sudo pacman -Syu --noconfirm lightdm lightdm-gtk-greeter lightdm-${init}
 sudo pacman -Syu --noconfirm mate
-sudo ln -s /etc/runit/sv/lightdm /run/runit/service
+
+if [ $init == runit ]
+then
+  echo Linking up DM service
+  sudo ln -s /etc/runit/sv/lightdm /run/runit/service
+else
+  echo You have to link up the lightdm service for $init
+fi
 
 echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
