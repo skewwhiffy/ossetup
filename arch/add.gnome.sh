aa15690f032aa06b2463ba566e4381c06c201e92
Kenny Hung <kennysubs+github@outlook.com>
2020-04-23 15:15:57 +0100

    Install gnome chrome shell with gnome in arch

--- arch/add.gnome.sh
+++ arch/add.gnome.sh
@@ -1,6 +1,6 @@
-sudo pacman -Syu
-sudo pacman -Syu linux-headers nvidia terminator \
-  xorg gnome gnome-extra
+yay -Syu --noconfirm --needed -Syu
+yay -Syu --noconfirm --needed -Syu linux-headers nvidia terminator \
+  xorg gnome gnome-extra chrome-gnome-shell
 sudo systemctl enable gdm
 
 echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
