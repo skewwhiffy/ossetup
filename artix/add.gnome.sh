886eec9565fe837543f910b2da03a76d5312ad04
Kenny Hung <kennysubs+github@outlook.com>
2020-04-24 09:22:21 +0100

    Use gdm for gnome in Artix

--- artix/add.gnome.sh
+++ artix/add.gnome.sh
@@ -2,7 +2,7 @@
 yay --noconfirm --needed -Syu
 yay --noconfirm --needed -Syu linux-headers nvidia terminator \
   xorg gnome gnome-extra chrome-gnome-shell
-yay -Syu --noconfirm --needed lightdm lightdm-gtk-greeter lightdm-runit
-sudo ln -s /etc/runit/sv/lightdm /run/runit/service
+yay -Syu --noconfirm --needed gdm gdm-runit
+sudo ln -s /etc/runit/sv/gdm /run/runit/service
 
 echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
