eb3a8be9864bce7ede2b5cc66fae3c31b10a98d2
Kenny Hung <kennysubs+github@outlook.com>
2020-05-09 11:25:41 +0100

    Make first.run.sh work for artix

--- artix/first.run.sh
+++ artix/first.run.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
+echo Enabling network
+sudo ln -s /etc/runit/sv/NetworkManager /run/runit/service
+
 echo Mounting DOCS drive
-sudo echo "LABEL=DOCS /media/Docs ext4 rw,relatime 0 1" >> /etc/fstab
+echo "LABEL=DOCS /media/Docs ext4 rw,relatime 0 1" | sudo tee -a /etc/fstab
 sudo mkdir -p /media/Docs
 sudo mount -L DOCS /media/Docs
 cp -r /media/Docs/backup/.ssh $HOME
 cp -r /media/Docs/backup/.aws $HOME
 
 echo Checking out ossetup repo
-mkdir -r $HOME/code/personal
+mkdir -p $HOME/code/personal
 cd $HOME/code/personal
 git clone git@github.com:skewwhiffy/ossetup.git
 cp /ossetup/artix/config ossetup/artix
