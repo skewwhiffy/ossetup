da83d7097a775a83e38334523b835d74bd12ac24
Kenny Hung <kennysubs+github@outlook.com>
2020-05-09 10:34:42 +0100

    Remove dm from cinnamon, and add icewm to Artix

--- artix/add.cinnamon.sh
+++ artix/add.cinnamon.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 sudo pacman -Syu --noconfirm --needed
-sudo pacman -Syu --noconfirm --needed xorg linux-headers nvidia-dkms terminator
-sudo pacman -Syu --noconfirm --needed lightdm lightdm-gtk-greeter lightdm-runit
+sudo pacman -Syu --noconfirm --needed xorg xorg-xinit linux-headers nvidia-dkms terminator
 sudo pacman -Syu --noconfirm --needed cinnamon
 yay -Syu --noconfirm --needed mint-themes redmond-themes-git
-sudo ln -s /etc/runit/sv/lightdm /run/runit/service
+echo cinnamon-session > $HOME/.xinitrc
 
-echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
+echo Reboot. Start cinnamon with startx.
---
+++ artix/add.icewm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo pacman -Syu --noconfirm --needed
+sudo pacman -Syu --noconfirm --needed xorg xorg-xinit linux-headers nvidia-dkms terminator
+sudo pacman -Syu --noconfirm --needed icewm
+echo icewm-session > $HOME/.xinitrc
+
+echo Reboot. Start x with startx.
