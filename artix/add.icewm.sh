9969b5cc2fff5b4d2e8397afb423888513b51bd0
Kenny Hung <kennysubs+github@outlook.com>
2020-05-09 10:48:24 +0100

    Add synapse to icewm

--- artix/add.icewm.sh
+++ artix/add.icewm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo pacman -Syu --noconfirm --needed
 sudo pacman -Syu --noconfirm --needed xorg xorg-xinit linux-headers nvidia-dkms terminator
-sudo pacman -Syu --noconfirm --needed icewm
+sudo pacman -Syu --noconfirm --needed icewm synapse
 echo icewm-session > $HOME/.xinitrc
 
 echo Reboot. Start x with startx.
