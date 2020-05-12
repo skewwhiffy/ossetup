5dd100bf04bbc0cc545b317f36069ab67f39bd08
Kenny Hung <kennysubs+github@outlook.com>
2020-05-12 16:49:15 +0100

    Add ksuperwin to xfce

--- artix/add.xfce.sh
+++ artix/add.xfce.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+./add.yay.sh
+
 sudo pacman -Syu --noconfirm --needed
 sudo pacman -Syu --noconfirm --needed xorg xorg-xinit linux-headers nvidia-dkms terminator
 sudo pacman -Syu --noconfirm --needed xfce4
+yay -Syu ksuperkey
 echo startxfce4 > $HOME/.xinitrc
 
 echo Reboot. Start x with startx.
