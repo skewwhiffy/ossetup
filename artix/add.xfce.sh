9d9a8f7e22dd0cadcd47ad4e0c4aee56727fc22e
Kenny Hung <kennysubs+github@outlook.com>
2020-05-13 10:06:38 +0100

    Add volume control plugin to xfce

--- artix/add.xfce.sh
+++ artix/add.xfce.sh
@@ -2,9 +2,9 @@
 ./add.yay.sh
 
 sudo pacman -Syu --noconfirm --needed
-sudo pacman -Syu --noconfirm --needed xorg xorg-xinit linux-headers nvidia-dkms \
-  terminator network-manager-applet pavucontrol
-sudo pacman -Syu --noconfirm --needed xfce4
+sudo pacman -Syu --noconfirm --needed xorg xorg-xinit linux-headers nvidia-dkms
+sudo pacman -Syu --noconfirm --needed terminator network-manager-applet pavucontrol xfce4-pulseaudio-plugin
+sudo pacman -Syu --noconfirm --needed xfce4 
 yay -Syu --noconfirm --needed ksuperkey
 echo startxfce4 > $HOME/.xinitrc
 
