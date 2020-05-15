c8b3f8e104253c3ff5a08b9f161572306aafc646
Kenny Hung <kennysubs+github@outlook.com>
2020-05-15 20:42:45 +0100

    Read choices for disk from user rather than rely on config file in pre.flight.checks

--- artix/pre.flight.checks.sh
+++ artix/pre.flight.checks.sh
@@ -2,22 +2,40 @@
 echo Checking for config
 if [ ! -f config ]
 then
-  echo "***** ERROR *****"
-  echo "No config file"
-  echo "Do you need to copy config.template over?"
-  exit 1
+  touch config
 fi
 
 echo Getting system disk
 export disk=$(grep -Po "(?<=^disk=).+" config)
-if [ $disk == SYSTEM_DISK ]
+if [ "$disk" == "" ]
 then
-  echo "***** ERROR *****"
-  echo "You need to set the system disk in config"
-  fdisk -l
-  exit 1
+  while true; do
+    echo "Please choose disk to install the operating system to"
+    sudo fdisk -l | grep Disk | grep sectors
+    export choices=$(sudo fdisk -l | grep Disk | grep sectors | sed "s/^Disk \(.*\):.*/\1/" )
+    i=1
+    for choice in $choices; do
+      echo [$i]: $choice
+      i=$((i+1))
+    done
+    read -p 'Your choice: ' choiceIndex
+    i=1
+    for choice in $choices; do
+      if [ "$choiceIndex" == $i ]; then
+        disk=$choice
+      fi
+      i=$((i+1))
+    done
+    if [ "$disk" != "" ]; then
+      break
+    fi
+    echo Your choice was not valid.
+    echo Please choose again.
+    echo
+  done
 fi
 echo System disk is $disk
+exit
 
 echo Getting init system
 export init=$(grep -Po "(?<=^init=).+" config)
