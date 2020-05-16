3375a5b630aedd49b545d7b3490819da32f03a97
Kenny Hung <kennysubs+github@outlook.com>
2020-05-16 10:59:28 +0100

    Remove old arch file

--- arch.old/add.cinnamon.sh
+++
@@ -1,7 +0,0 @@
-sudo pacman -Syu --noconfirm
-sudo pacman -Syu --noconfirm linux-headers nvidia terminator
-sudo pacman -Syu --noconfirm lightdm lightdm-gtk-greeter
-sudo pacman -Syu --noconfirm cinnamon
-sudo systemctl enable lightdm
-
-echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
--- arch.old/add.gnome.sh
+++
@@ -1,6 +0,0 @@
-yay --noconfirm --needed -Syu
-yay --noconfirm --needed -Syu linux-headers nvidia terminator \
-  xorg gnome gnome-extra chrome-gnome-shell
-sudo systemctl enable gdm
-
-echo Reboot. You should get a greeter. Login. Run add.common.sh to install common apps.
