737594bee9fe2cfc7f6901198ca3a9fcfaf89598
Kenny Hung <kennysubs+github@outlook.com>
2020-06-14 20:44:33 +0100

    Remove auto-generated fish config before init yadm

--- arch/add.vital.sh
+++ arch/add.vital.sh
@@ -15,6 +15,7 @@ toInstall=(
 sudo chsh -s /usr/bin/fish
 sudo chsh $user -s /usr/bin/fish
 
+rm -rf ~/.config/fish/*
 yadm init
 yadm remote add origin git@github.com:skewwhiffy/yadm.git
 yadm fetch
