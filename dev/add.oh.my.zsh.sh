78b405fc5fc1a176c718baecdc5d3612a4e552ee
Kenny Hung <kennysubs+github@outlook.com>
2020-06-14 09:43:27 +0100

    Reinstate zsh, remove oh.my.zsh

--- dev/add.oh.my.zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo Installing oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo Installing zinit
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
-echo Installing zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
