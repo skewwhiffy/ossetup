14c15d2c8046de663dc2ab811ec4fb90fc7c0b7c
Kenny Hung <kennysubs+github@outlook.com>
2020-06-17 13:51:14 +0100

    Install go plugin in vs code

--- dev/add.code.plugins.sh
+++ dev/add.code.plugins.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 code --install-extension dcortes92.FreeMarker
 code --install-extension GabrielBB.vscode-lombok
-code --install-extension ms-vscode.Go
 code --install-extension redhat.java
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscjava.vscode-java-debug
@@ -9,3 +8,4 @@ code --install-extension vscjava.vscode-java-dependency
 code --install-extension vscjava.vscode-java-pack
 code --install-extension vscjava.vscode-maven
 code --install-extension vscodevim.vim
+code --install-extension golang.go
