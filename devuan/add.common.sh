7e698ef28c79f0a18283c1ba7e68405194cfad84
Kenny Hung <kennysubs+github@outlook.com>
2020-06-25 14:14:23 +0100

    Install neovim on devuan

--- devuan/add.common.sh
+++ devuan/add.common.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
 
 echo Installing common stuff
-sudo apt -y install neovim \
+sudo apt -y install \
+  cmake \
+  nautilus-dropbox \
+  keepassxc \
+  automake \
+  libtool \
+  libtool-bin \
+  gettext \
   xclip \
   fish \
   yadm \
   curl \
   wget \
   chromium \
-  fcitx-libpinyin fcitx-config-gtk \
+  fcitx-libpinyin \
+  fcitx-config-gtk \
   silversearcher-ag \
   gnome-disk-utility \
   gnome-system-monitor \
   gnome-screenshot \
   rhythmbox \
-  maven python python-pip python2 python2-pip \
+  maven python3-pip python3-pip \
   emacs gitk \
   bison golang \
   mame \
-  # cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - FOR DROPBOX
-# Enable: https://devuan.org/os/etc/apt/sources.list backports
+  xdotool \
+  wmctrl
+
+echo Installing nvim
+mkdir -p ~/code/third.party
+if [ ! -d ~/code/third.party/neovim ]; then
+  cd ~/code/third.party
+  git clone https://github.com/neovim/neovim
+  cd neovim
+  make CMAKE_BUILD_TYPE=RelWithDebInfo
+  sudo make install
+fi
+sudo update-alternatives --install /etc/alternatives/nvim editor /usr/local/bin/nvim 30
+
 # jetbrains toolbox
 # curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
 
