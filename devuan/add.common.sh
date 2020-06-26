166b746b7d149f716d18ce846b9f1eb8061dcada
Kenny Hung <kennysubs+github@outlook.com>
2020-06-26 10:18:52 +0100

    Network manager, and switch to an rtl8812au driver that works

--- devuan/add.common.sh
+++ devuan/add.common.sh
@@ -1,61 +1,96 @@
 #!/usr/bin/env bash
+echo Installing vital stuff
+sudo apt -y install \
+  tmux \
+  yadm \
+  fish \
+  curl \
+  wget
+
+sudo apt -y install \
+  terminator \
+  nautilus-dropbox \
+  keepassxc \
+  gnome-disk-utility \
+  python3-pip \
 
 echo Installing common stuff
 sudo apt -y install \
+  dkms \
+  docker.io \
+  make \
+  libx11-dev \
+  libxtst-dev \
+  pkg-config \
   cmake \
-  nautilus-dropbox \
-  keepassxc \
   automake \
   libtool \
   libtool-bin \
   gettext \
   xclip \
-  fish \
-  yadm \
-  curl \
-  wget \
-  chromium \
   fcitx-libpinyin \
   fcitx-config-gtk \
   silversearcher-ag \
-  gnome-disk-utility \
   gnome-system-monitor \
   gnome-screenshot \
   rhythmbox \
-  maven python3-pip python3-pip \
-  emacs gitk \
-  bison golang \
+  maven \
+  emacs \
+  gitk \
+  bison \
+  gcc \
   mame \
   xdotool \
-  wmctrl
+  wmctrl \
+  network-manager
 
-echo Installing nvim
+sudo usermod -aG docker $(whoami)
+
+echo Installing third party software
 mkdir -p ~/code/third.party
-if [ ! -d ~/code/third.party/neovim ]; then
+
+if ! type -P nvim &> /dev/null; then
+  echo Installing nvim
   cd ~/code/third.party
+  rm -rf neovim
   git clone https://github.com/neovim/neovim
   cd neovim
   make CMAKE_BUILD_TYPE=RelWithDebInfo
   sudo make install
+  sudo update-alternatives --install /etc/alternatives/nvim editor /usr/local/bin/nvim 30
 fi
-sudo update-alternatives --install /etc/alternatives/nvim editor /usr/local/bin/nvim 30
 
-# jetbrains toolbox
-# curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+if ! type -P ksuperkey &> /dev/null; then
+  echo Installing ksuperkey
+  cd ~/code/third.party
+  git clone https://github.com/hanschen/ksuperkey
+  cd ksuperkey
+  make
+  sudo make install
+fi
 
-#yay -Syu --noconfirm --needed authy https://aur.archlinux.org/packages/authy/
+if [ ! -d ~/code/third.party/rtl8812au ]; then
+  echo Installing RTL 8812 AU driver
+  cd ~/code/third.party
+  git clone https://github.com/gnab/rtl8812au.git
+  cd rtl8812au
+  make
+  sudo cp 8812au.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless
+  sudo depmod
+fi
+
+if ! type -P codium &> /dev/null; then
+  echo Installing Codium
+  wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
+  echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
+  sudo apt update && sudo apt -y install codium
+fi
+
 #yay -Syu --noconfirm --needed code https://github.com/VSCodium/vscodium/releases
 #yay -Syu --noconfirm --needed dbeaver https://dbeaver.io/download/
-#yay -Syu --noconfirm --needed docker
-#yay -Syu --noconfirm --needed docker-${init}
-#yay -Syu --noconfirm --needed hplip
-#yay -Syu --noconfirm --needed ifconfig
-#yay -Syu --noconfirm --needed inetutils
-#yay -Syu --noconfirm --needed jetbrains-toolbox
 #yay -Syu --noconfirm --needed mongodb-compass
 #yay -Syu --noconfirm --needed mongodb-shell-bin
 #yay -Syu --noconfirm --needed mongodb-tools-bin
-#yay -Syu --noconfirm --needed net-tools
 #yay -Syu --noconfirm --needed noto-fonts
 #yay -Syu --noconfirm --needed noto-fonts-cjk
 #yay -Syu --noconfirm --needed noto-fonts-emoji
---
+++ devuan/add.extras.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo Pay attention!
+sudo apt -y install \
+  intel-microcode \
+  nvidia-driver
