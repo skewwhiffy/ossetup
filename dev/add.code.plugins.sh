c594c7d74477e8c278415314a89122d79d68a956
Kenny Hung <kennysubs+github@outlook.com>
2020-06-27 23:09:09 +0100

    Fix codium install

--- dev/add.code.plugins.sh
+++ dev/add.code.plugins.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
-code --install-extension dcortes92.FreeMarker
-code --install-extension GabrielBB.vscode-lombok
-code --install-extension redhat.java
-code --install-extension VisualStudioExptTeam.vscodeintellicode
-code --install-extension vscjava.vscode-java-debug
-code --install-extension vscjava.vscode-java-dependency
-code --install-extension vscjava.vscode-java-pack
-code --install-extension vscjava.vscode-maven
-code --install-extension vscodevim.vim
-code --install-extension golang.go
+if type -P code &> /dev/null; then
+  codeCommand=code
+fi
+
+if type -P codium &> /dev/null; then
+  codeCommand=codium
+fi
+
+if [ "$codeCommand" == "" ]; then
+  echo Code not installed.
+  exit 1
+fi
+
+eval "$codeCommand --install-extension dcortes92.FreeMarker"
+eval "$codeCommand --install-extension GabrielBB.vscode-lombok"
+eval "$codeCommand --install-extension redhat.java"
+eval "$codeCommand --install-extension VisualStudioExptTeam.vscodeintellicode"
+eval "$codeCommand --install-extension vscjava.vscode-java-debug"
+eval "$codeCommand --install-extension vscjava.vscode-java-dependency"
+eval "$codeCommand --install-extension vscjava.vscode-java-pack"
+eval "$codeCommand --install-extension vscjava.vscode-maven"
+eval "$codeCommand --install-extension vscodevim.vim"
+eval "$codeCommand --install-extension golang.go"
--- mint/add.common.sh
+++ mint/add.common.sh
@@ -84,7 +84,7 @@ if ! type -P codium &> /dev/null; then
   echo Installing Codium
   wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
   echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
-  sudo apt update && sudo apt -y install codium
+  sudo apt update && sudo apt install -y codium
 fi
 
 #yay -Syu --noconfirm --needed code https://github.com/VSCodium/vscodium/releases
