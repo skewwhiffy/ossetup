e04f7a21652129acde9d48be42fdee6624409e81
Kenny Hung <kennysubs+github@outlook.com>
2020-07-04 14:22:08 +0100

    Audio stuff for Void

--- void/add.common.sh
+++ void/add.common.sh
@@ -8,4 +8,6 @@ sudo xbps-install -Syu \
   keepassxc \
   nautilus-dropbox \
   xfce4-whiskermenu-plugin xfce4-screensaver \
-  xdpyinfo xdotool wmctrl xwininfo
+  xdpyinfo xdotool wmctrl xwininfo \
+  apache-maven python3-pip \
+  rhythmbox pulseaudio xfce4-pulseaudio-plugin
