3ec2858c5881ed96fcaaf3ed13bd7a42da94b3ed
Kenny Hung <kennysubs+github@outlook.com>
2020-11-17 13:14:48 +0000

    Create XFCE config folder if not exists

--- xfce/set.keyboard.shortcuts.sh
+++ xfce/set.keyboard.shortcuts.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml
 cp ~/code/personal/ossetup/xfce/resources/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
