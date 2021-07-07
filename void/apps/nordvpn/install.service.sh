2992361f7727b05ee872fe74c589114862fac6dd
Kenny Hung <kennysubs+github@outlook.com>
2021-07-07 18:14:50 +0100

    Remove nordvpn for Void

--- void/apps/nordvpn/install.service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo I might need a sudo password
-sudo rm -f /var/service/nordvpn
-sudo rm -rf /etc/sv/nordvpn
-sudo mkdir /etc/sv/nordvpn
-sudo cp -rp ./service/** /etc/sv/nordvpn
-sudo chmod 0755 -R /etc/sv/nordvpn
-sudo ln -s /etc/sv/nordvpn /var/service
