59592a3c7fb996a26087ea04e13ed2690d5c3194
Kenny Hung <kennysubs+github@outlook.com>
2021-08-02 16:30:37 +0100

    Remove dev tools. Don't even know if they still work

--- dev/add.g.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo Installing g
-curl -sSL https://git.io/g-install | sh -s -- fish -y
-# curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
