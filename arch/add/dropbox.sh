fbe9e005a8e4f3091cc6f088a66b9e2e91090772
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-09-18 19:11:29 +0100

    Remove arch dropbox script

--- arch/add/dropbox.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-yay -Syu --noconfirm wget
-wget https://linux.dropbox.com/fedora/rpm-public-key.asc
-gpg --import rpm-public-key.asc
-rm rpm-public-key.asc
-yay -Syu --noconfirm --needed dropbox
